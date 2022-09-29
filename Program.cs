2932f153ba695ba639e6fcc60577b1fcb2659770
i1lness <shdxrvet@gmail.com>
2022-09-30 08:36:01 +0900

    alpha 0.1.1 (2022-09-30) 구문 최적화 및 기능 함수화

--- Program.cs
+++ Program.cs
@@ -4,20 +4,45 @@ namespace CSharp
 {
     class Program
     {
-        static void Main(string[] args)
+        enum JobType
         {
-            bool isJobSelectTrue = false;
+            None = 0,
+            Knight = 1,
+            Archer = 2,
+            Mage = 3
+        }
 
-            while(!isJobSelectTrue)
+        JobType SelectJob()
+        {
+            JobType jobType = JobType.None;
+
+            while (jobType == JobType.None)
             {
                 Console.WriteLine("직업을 선택하세요!\n[1] 기사\n[2] 궁수\n[3] 마법사");
-                int jobSelectNum = Convert.ToInt32(Console.ReadLine());
-                if (jobSelectNum == 1 || jobSelectNum == 2 || jobSelectNum == 3)
+                string? input = Console.ReadLine();
+
+                switch (input)
                 {
-                    isJobSelectTrue = true;
-                    break;
+                    case "1":
+                        jobType = JobType.Knight;
+                        break;
+                    case "2":
+                        jobType = JobType.Archer;
+                        break;
+                    case "3":
+                        jobType = JobType.Mage;
+                        break;
                 }
             }
+
+            return jobType;
+        }
+
+        static void Main(string[] args)
+        {
+            JobType jobType;
+            Program p = new Program();
+            jobType = p.SelectJob();
 
         }
     }
