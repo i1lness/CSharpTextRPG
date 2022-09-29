eb7b191307a75244c18b858f84718d97dbd62545
i1lness <shdxrvet@gmail.com>
2022-09-30 07:17:50 +0900

    alpha 0.1.0 (2022-09-30) 직업 선택 시스템 추가

--- Program.cs
+++ Program.cs
@@ -6,6 +6,18 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
+            bool isJobSelectTrue = false;
+
+            while(!isJobSelectTrue)
+            {
+                Console.WriteLine("직업을 선택하세요!\n[1] 기사\n[2] 궁수\n[3] 마법사");
+                int jobSelectNum = Convert.ToInt32(Console.ReadLine());
+                if (jobSelectNum == 1 || jobSelectNum == 2 || jobSelectNum == 3)
+                {
+                    isJobSelectTrue = true;
+                    break;
+                }
+            }
 
         }
     }
